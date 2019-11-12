cbe9570a3265b5d1aee2bc77a933f8cb771531de
GuyARoss <guyallenross@gmail.com>
2019-11-11 20:30:43 -0500

    made app more moduler.

---
+++ src/components/graphs/line.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import { Line } from 'react-chartjs-2';
+
+export default ({ title, data }) => (
+    <Line data={{
+        labels: [],
+        datasets: [{
+            label: title,
+            backgroundColor: 'rgba(255,99,132,0.2)',
+            borderColor: 'rgba(255,99,132,1)',
+            borderWidth: 1,
+            hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+            hoverBorderColor: 'rgba(255,99,132,1)',
+            data,
+        }]
+    }} options={{
+        scales: {
+            xAxes: [{
+                gridLines: {
+                    display: false,
+                },
+            }],
+            yAxes: [{
+                gridLines: {
+                    display: true,
+                    color: 'rgba(45,50,73,1)',
+                    lineWidth: 2,
+                    drawTicks: false,
+                    drawBorder: false,
+                },
+            }],
+        }
+    }} />
+)
--- src/containers/live/index.js
+++ src/containers/live/index.js
@@ -1,191 +1,48 @@
-import React, { useState, useMemo, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+
+import { Container, Row } from 'react-bootstrap';
 
 import manifest from '../../config/manifest';
 import { connectSocket } from '../../helpers';
-
-import { Line } from 'react-chartjs-2';
-import { Container, Row, Col, Card } from 'react-bootstrap';
+import GraphContainer from '../../components/container';
+import { LineGraph } from '../../components/graphs';
 
 import './index.css';
 
 const connectToLiveSocket = (appendCb) => connectSocket(
-    `${manifest.hostMonitor}/live`,
-    (live) => {
-        live.emit('subscribeToLiveTransmission', ['duh', '136.60.227.124', '157.230.154.30']);
-        live.on('liveTransmission', (transmissionPacket) => appendCb(transmissionPacket));
-    });
+  `${manifest.hostMonitor}/live`,
+  (live) => {
+    live.emit('subscribeToLiveTransmission', ['duh', '136.60.227.124', '157.230.154.30']);
+    live.on('liveTransmission', (transmissionPacket) => appendCb(transmissionPacket));
+  });
 
 export default () => {
-    const [transmissionPackets, setTransmissionPackets] = useState([]);
-    useEffect(() => {
-        connectToLiveSocket(( transmissionPacket ) => {
-            setTransmissionPackets([...transmissionPackets, transmissionPacket]);
-        })
-    }, []);
-    return (
-        <div>
-        <Container fluid="true" className="main-cont py-5">
+  const [transmissionPackets, setTransmissionPackets] = useState({
+    CpuUsage: [],
+    MemoryTotal: [],
+  });
+
+  useEffect(() => {
+    connectToLiveSocket(({ CpuUsage, MemoryTotal }) => {     
+      setTransmissionPackets({
+        CpuUsage: [...transmissionPackets.CpuUsage, CpuUsage],
+        MemoryTotal: [...transmissionPackets.MemoryTotal, MemoryTotal],
+      })
+    })
+  }, []);
+
+  return (
+    <div>
+      <Container fluid="true" className="main-cont py-5">
         <Row className="">
-          <Col className="mx-auto mt-5" sm={10} md={6}>
-            <Card className="dark-card text-center">
-            <Card.Header className="dark-card-header">
-              <h1>CPU</h1>
-            </Card.Header>
-            <Card.Body>
-            <Line data={{
-              labels: [],
-                datasets: [{
-                  label: 'My First dataset',
-                  backgroundColor: 'rgba(255,99,132,0.2)',
-                  borderColor: 'rgba(255,99,132,1)',
-                  borderWidth: 1,
-                  hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-                  hoverBorderColor: 'rgba(255,99,132,1)',
-                  data: transmissionPackets,
-                }]
-            }}
-            options={{
-              scales: {
-              xAxes: [{
-                gridLines: {
-                  display: false,
-                },
-              }],
-              yAxes: [{
-                gridLines: {
-                  display: true,
-                  color: 'rgba(45,50,73,1)',
-                  lineWidth: 2,
-                  drawTicks: false,
-                  drawBorder: false,
-                },
-              }],
-              },
-            }}
-            />
-            </Card.Body>
-            <Card.Footer className="text-muted">PUT FILTERS HERE</Card.Footer>
-            </Card>
-          </Col>
-          <Col className="mx-auto mt-5" sm={10} md={6}>
-            <Card className="dark-card text-center">
-              <Card.Header className="dark-card-header">
-                <h1>Network</h1>
-              </Card.Header>
-              <Card.Body>
-                <Line data={{
-                  labels: [],
-                    datasets: [{
-                      label: 'My First dataset',
-                      backgroundColor: 'rgba(255,99,132,0.2)',
-                      borderColor: 'rgba(255,99,132,1)',
-                      borderWidth: 1,
-                      hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-                      hoverBorderColor: 'rgba(255,99,132,1)',
-                      data: transmissionPackets,
-                    }]
-                }} options={{
-                    scales: {
-                    xAxes: [{
-                      gridLines: {
-                        display: false,
-                      },
-                    }],
-                    yAxes: [{
-                      gridLines: {
-                        display: true,
-                        color: 'rgba(45,50,73,1)',
-                        lineWidth: 2,
-                        drawTicks: false,
-                        drawBorder: false,
-                      },
-                    }],
-                    }
-                }} />
-              </Card.Body>
-              <Card.Footer className="text-muted">PUT FILTERS HERE</Card.Footer>
-            </Card>
-          </Col>
-          <Col className="mx-auto mt-5" sm={10} md={6}>
-            <Card className="dark-card text-center">
-              <Card.Header className="dark-card-header">
-                <h1>Network</h1>
-              </Card.Header>
-              <Card.Body>
-                <Line data={{
-                  labels: [],
-                    datasets: [{
-                      label: 'My First dataset',
-                      backgroundColor: 'rgba(255,99,132,0.2)',
-                      borderColor: 'rgba(255,99,132,1)',
-                      borderWidth: 1,
-                      hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-                      hoverBorderColor: 'rgba(255,99,132,1)',
-                      data: transmissionPackets,
-                    }]
-                }} options={{
-                    scales: {
-                    xAxes: [{
-                      gridLines: {
-                        display: false,
-                      },
-                    }],
-                    yAxes: [{
-                      gridLines: {
-                        display: true,
-                        color: 'rgba(45,50,73,1)',
-                        lineWidth: 2,
-                        drawTicks: false,
-                        drawBorder: false,
-                      },
-                    }],
-                    }
-                }} />
-              </Card.Body>
-              <Card.Footer className="text-muted">PUT FILTERS HERE</Card.Footer>
-            </Card>
-          </Col>
-          <Col className="mx-auto mt-5" sm={10} md={6}>
-            <Card className="dark-card text-center">
-              <Card.Header className="dark-card-header">
-                <h1>Network</h1>
-              </Card.Header>
-              <Card.Body>
-                <Line data={{
-                  labels: [],
-                    datasets: [{
-                      label: 'My First dataset',
-                      backgroundColor: 'rgba(255,99,132,0.2)',
-                      borderColor: 'rgba(255,99,132,1)',
-                      borderWidth: 1,
-                      hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-                      hoverBorderColor: 'rgba(255,99,132,1)',
-                      data: transmissionPackets,
-                    }]
-                }} options={{
-                    scales: {
-                    xAxes: [{
-                      gridLines: {
-                        display: false,
-                      },
-                    }],
-                    yAxes: [{
-                      gridLines: {
-                        display: true,
-                        color: 'rgba(45,50,73,1)',
-                        lineWidth: 2,
-                        drawTicks: false,
-                        drawBorder: false,
-                      },
-                    }],
-                    }
-                }} />
-              </Card.Body>
-              <Card.Footer className="text-muted">PUT FILTERS HERE</Card.Footer>
-            </Card>
-          </Col>
+          <GraphContainer title="Cpu">
+            <LineGraph data={transmissionPackets.CpuUsage} title="Cpu" />
+          </GraphContainer>
+          <GraphContainer title="Memory">
+            <LineGraph data={transmissionPackets.MemoryTotal} title="Network" />
+          </GraphContainer>
         </Row>
-        </Container>
-        </div>
-    )
+      </Container>
+    </div>
+  )
 }
