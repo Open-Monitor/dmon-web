a2b39a5f02549cc701984d2f69838f7da79039b1
Guy Ross <guyallenross@gmail.com>
2019-11-12 23:37:59 -0500

    (chore): removed connect socket from helpers.

--- src/helpers/connect-socket.js
+++
@@ -1,6 +0,0 @@
-import io from 'socket.io-client';
-
-export default (url, cb) => {
-    const socket = io(url);
-    socket.on('connect', () => cb(socket))
-}
--- src/helpers/index.js
+++
@@ -1,1 +0,0 @@
-export {default as connectSocket} from './connect-socket';
